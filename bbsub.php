cb2c7229790d90a43354bb7b1763e6a2ba990907
Ryan McCue <me@ryanmccue.info>
2015-04-12 20:43:30 +1000

    Refactor loader

--- bbsub.php
+++ bbsub.php
@@ -12,55 +12,52 @@ Author: Ryan McCue
 Author URI: http://ryanmccue.info/
 */
 
-class bbSub {
-	public static $path;
-
-	public static function load() {
-		self::$path = __DIR__;
-		spl_autoload_register(array(get_called_class(), 'autoload'));
-		bbSubscriptions::bootstrap();
-	}
-
-	public static function report_error() {
-		echo '<div class="error"><p>Please install &amp; activate Sputnik to enable bbSubscriptions.</p></div>';
-	}
-
-	/**
-	 * Register cron event on activation
-	 */
-	public static function activation() {
-		wp_schedule_event(time(), 'bbsub_minutely', 'bbsub_check_inbox');
-	}
-
-	/**
-	 * Clear cron event on deactivation
-	 */
-	public static function deactivation() {
-		wp_clear_scheduled_hook('bbsub_check_inbox');
-	}
-
-	public static function autoload($class) {
-		if (strpos($class, 'EmailReplyParser') === 0) {
-			$filename = str_replace(array('_', '\\'), '/', $class);
-			$filename = self::$path . '/vendor/EmailReplyParser/src/' . $filename . '.php';
-			if (file_exists($filename)) {
-				require_once($filename);
-			}
-			return;
-		}
-		if (strpos($class, 'bbSubscriptions') !== 0) {
-			return;
-		}
-
-		$filename = str_replace(array('_', '\\'), '/', $class);
-		$filename = self::$path . '/library/' . $filename . '.php';
-		if (file_exists($filename)) {
-			require_once($filename);
-		}
-	}
+if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	echo '<p>bbPress Subscriptions requires PHP 5.3 or newer.</p>';
+	exit;
 }
 
-register_activation_hook(__FILE__, array('bbSub', 'activation'));
-register_deactivation_hook(__FILE__, array('bbSub', 'deactivation'));
+register_activation_hook( __FILE__, 'bbsub_activation' );
+register_deactivation_hook( __FILE__, 'bbsub_deactivation' );
 
-add_action('plugins_loaded', array('bbSub', 'load'));
+add_action( 'plugins_loaded', 'bbsub_load' );
+
+function bbsub_load() {
+	define( 'BBSUB_PATH', __DIR__ );
+	spl_autoload_register( 'bbsub_autoload' );
+	bbSubscriptions::bootstrap();
+}
+
+/**
+ * Register cron event on activation
+ */
+function bbsub_activation() {
+	wp_schedule_event( time(), 'bbsub_minutely', 'bbsub_check_inbox' );
+}
+
+/**
+ * Clear cron event on deactivation
+ */
+function bbsub_deactivation() {
+	wp_clear_scheduled_hook( 'bbsub_check_inbox' );
+}
+
+function bbsub_autoload($class) {
+	if ( strpos( $class, 'EmailReplyParser' ) === 0 ) {
+		$filename = str_replace( array( '_', '\\' ), '/', $class );
+		$filename = self::$path . '/vendor/EmailReplyParser/src/' . $filename . '.php';
+		if ( file_exists( $filename ) ) {
+			require_once( $filename );
+		}
+		return;
+	}
+	if ( strpos( $class, 'bbSubscriptions' ) !== 0 ) {
+		return;
+	}
+
+	$filename = str_replace( array( '_', '\\' ), '/', $class );
+	$filename = self::$path . '/library/' . $filename . '.php';
+	if ( file_exists( $filename ) ) {
+		require_once( $filename );
+	}
+}
